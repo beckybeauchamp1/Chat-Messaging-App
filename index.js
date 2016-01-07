c71b68427edf26686bedf96f6e0f863386dfd0e9
Becky Beauchamp <beckybeauchamp1@gmail.com>
2016-01-06 19:04:14 -0500

    Add UserName Display

--- index.js
+++ index.js
@@ -6,14 +6,19 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
-  res.sendfile('index.html');
-});
+app.use(express.static(__dirname + '/public'));
 
-io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
+io.on("connection", function(socket){
+  socket.on("addingUser", function(name){
+    io.emit("showUserEntered", name);
+  });
+  socket.on('messageToServer', function(msg){
+    io.emit("messageToClient", msg);
+    console.log(msg.username + ' said:' + msg.message);
+  });
+  socket.on('disconnect', function(data){
+      console.log(data);
+      socket.emit("showDisconnectMsg", data);
   });
 });
 
--- public/script.js
+++ public/script.js
@@ -1,2 +1,42 @@
 // loading the socket.io client into our application, exposes a io global object in the browser then connecting
- var socket = io();
+$(document).ready(function(){
+  var socket = io();
+
+  $(".sendName").on("click", function(){
+    username = $(".grabName").val();
+    $(".name").hide();
+    $(".message-form").css("display", "inline");
+    socket.emit("addingUser", username);
+  });
+
+  socket.on("showUserEntered", function(username){
+    $("h1").append($("<p class='show-username'></p>").text(username + " has entered the Chatroom!"));
+    setTimeout(function(){
+      $(".show-username").fadeOut();
+    }, 2000);
+  });
+
+  socket.on("showDisconnectMsg", function(username){
+    $("h1").append($("<p class='show-username'></p>").text(username + " has left the Chatroom!"));
+    setTimeout(function(){
+      $(".show-username").fadeOut();
+    }, 2000);
+  });
+
+  $(".send-button").on("click", function(){
+    var msg= $("#msg").val();
+    socket.emit("messageToServer", {
+      username: username,
+      message: msg
+    });
+    $("#msg").val("");
+    return false;
+  });
+
+  socket.on("messageToClient", function(msg){
+    $(".messages").append($("<li></li>").text(msg.username + ": " + msg.message));
+  });
+
+
+
+});
