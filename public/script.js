be6ed71d0bf4f242fce3b5158813917ec3f5941a
Becky Beauchamp <beckybeauchamp1@gmail.com>
2016-01-06 23:16:47 -0500

    Add Git Ignor

--- public/script.js
+++ public/script.js
@@ -1,2 +1,7 @@
 // loading the socket.io client into our application, exposes a io global object in the browser then connecting
- var socket = io();
+var socket = io();
+
+$(document).ready(function(){
+
+
+});
